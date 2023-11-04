d17e455a0b842980c138f33b513d24dda32d9506
Pietro Duduch <pietro.duduch@gmail.com>
2023-11-04 16:04:44 -0300

    Event System Implementation

---
+++ Unity Metal Detector/Assets/Scripts/Events/GameEvent.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameEvent")]
+public class GameEvent : ScriptableObject
+{
+   public List<GameEventListener> listeners = new List<GameEventListener>();
+
+    public void Raise(Component sender, object data)
+    {
+        for (int i = 0; i < listeners.Count; i++)
+        {
+            listeners[i].OnEventRaised(sender, data);
+        }
+    }
+
+    public void RegisterListener(GameEventListener listener)
+    {
+        if (!listeners.Contains(listener))
+        {
+            listeners.Add(listener);
+        }
+    }
+
+    public void UnregisterListener(GameEventListener listener)
+    {
+        if(listeners.Contains(listener))
+        {
+            listeners.Remove(listener);
+        }
+    }
+}
